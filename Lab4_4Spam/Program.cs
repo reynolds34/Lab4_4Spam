0622cf0b52fe5565a27f78990b53580fc9cc4d51
Bradley Reynolds <breynolds3@my.ancoraed.com>
2020-02-25 16:33:20 -0600

    Add project files.

---
+++ Lab4_4Spam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab4_4Spam
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] blacklistWords = {
+            "buy", "viagra", "XXX", "free money", "free",
+                "lifetime offer", "send money", "bank account",
+                "nigeria", "online pharmacy", "h8te", "meet girls" };
+
+            string message = Console.ReadLine();
+            bool isSpam = false;
+            message = message.ToLower();
+            for (int i = 0; i < blacklistWords.Length; i++)
+            {
+                if (message.Contains(blacklistWords[i]))
+                {
+                    isSpam = true;
+                }
+            }
+
+
+            if (isSpam == true)
+            {
+                Console.WriteLine("The message contained spam");
+            }
+            else
+            { 
+                Console.WriteLine("The message does not contain spam.");
+            }
+        }
+    }
+}
